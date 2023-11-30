62bb0115bf9330eecad8fe5c329028dd70153368
Abdullah Alghamdi <61493052+xlizro@users.noreply.github.com>
2023-11-30 07:37:22 +0300

    fixing bugs and testing

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,12 +3,14 @@ import { Router } from 'express'
 /*======= Internal Modules or Files =======*/
 // Controllers
 import { loginUser, logoutUser } from '../controllers/authController'
+// Middlewares
+import { isLoggedOut, isLoggedIn } from '../middlewares/auth'
 
 const router = Router()
 
 // POST : /auth/login -> Login User
-router.post('/login', loginUser)
+router.post('/login', isLoggedOut, loginUser)
 // POST : /auth/logout -> Logout User
-router.post('/logout', logoutUser)
+router.post('/logout', isLoggedIn, logoutUser)
 
 export default router
